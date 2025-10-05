bef51a2a312bf59536d9cc51cafa072556c512b9
atharva <atharva123king@gmail.com>
2025-10-05 15:19:02 +0530

    add CMakeLists

---
+++ CMakeFiles/web_server.dir/link.d
@@ -0,0 +1,22 @@
+web_server: \
+ /data/data/com.termux/files/usr/lib/crtbegin_dynamic.o \
+ CMakeFiles/web_server.dir/src/main.c.o \
+ /data/data/com.termux/files/usr/lib/clang/20/lib/linux/libclang_rt.builtins-aarch64-android.a \
+ /data/data/com.termux/files/usr/lib/libunwind.a \
+ /system/lib64/libdl.so \
+ /system/lib64/libc.so \
+ /data/data/com.termux/files/usr/lib/crtend_android.o
+
+/data/data/com.termux/files/usr/lib/crtbegin_dynamic.o:
+
+CMakeFiles/web_server.dir/src/main.c.o:
+
+/data/data/com.termux/files/usr/lib/clang/20/lib/linux/libclang_rt.builtins-aarch64-android.a:
+
+/data/data/com.termux/files/usr/lib/libunwind.a:
+
+/system/lib64/libdl.so:
+
+/system/lib64/libc.so:
+
+/data/data/com.termux/files/usr/lib/crtend_android.o:
